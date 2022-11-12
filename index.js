169005d759ae76763dd1ae39f4a622e7b846af0c
sfrankhouser <113480868+sfrankhouser@users.noreply.github.com>
2022-11-12 15:53:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "./images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "./images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
